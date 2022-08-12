9a567adba2d00ca309a227b313aee94580353462
Karim Kiel <karim.kiel@slxware.de>
2022-08-12 09:38:25 +0200

    First commit

---
+++ src/pages/bio.php
@@ -0,0 +1,51 @@
+<title>Bio - Metal Fortress</title>
+<div class="uberschrift">Bio</div>
+    
+        <table class="tablegear">
+            <tr><td class="schrift-fetter schrift-blau"><u>General:</u></td><td class="schrift-klein">(Updated: 29.07.2022)</td></tr>
+            <tr> <td>name:</td> <td> Karim</td> </tr>
+            <tr> <td>born:</td> <td> Germany</td> </tr>
+            <tr> <td>age:</td> <td>
+                <?php
+                function alter ($datum) {
+                    $geburtstag = new DateTime($datum);
+                    $heute = new DateTime(date('Y-m-d'));
+                    $differenz = $geburtstag->diff($heute);
+                    return $differenz->format('%y');
+                } print alter('1996-09-30'); ?>
+            </td></tr>
+            <tr> <td>height:</td> <td> 189cm / 6.2 ft</td> </tr>
+            <tr> <td>hobbies:</td> <td> cycling, music, computers, video games</td> </tr>
+            <tr> <td>languages:</td> <td> german (native)<br>english (learned in school)<br>french (just for fun)<br>planned: dutch, tagalog</td> </tr>
+            <tr> <td>studied:</td> <td> Java Programming</td> </tr>
+            <tr> <td>currently:</td> <td> apprenticeship as IT specialist for system integration</td> </tr>
+            <tr> <td></td><td></td></tr>
+            <tr><td class="schrift-fetter schrift-blau"><u>Favorite:</u></td><td class="schrift-klein">(Updated: 17.06.2022)</td></tr>
+            <tr> <td>guitar:</td> <td> <a href="https://www.fmicassets.com/Damroot/ZoomJpg/10002/0134444780_gtr_frt_001_rr.jpg" target="_blank">Jim Root Telecaster</a></td> </tr>
+            <tr> <td>band:</td> <td> Currently: <a href="https://www.youtube.com/c/FirstToEleven" target="_blank">First of Eleven</a></td> </tr>
+            <tr> <td>guitarist:</td> <td> <a href="https://www.youtube.com/user/mattrach" target="_blank">Mattrach</a></td> </tr>
+            <tr> <td>producer:</td> <td> <a href="https://www.youtube.com/user/OfficialVirtualRiot" target="_blank">Virtual Riot</a></td> </tr>
+            <tr> <td>pianist:</td> <td> <a href="https://youtu.be/9DyHFonnu2E?t=10" target="_blank">Tom Brier</a></td> </tr>
+            <tr> <td>song:</td> <td> Currently: <a href="https://www.youtube.com/watch?v=um_cAPmL4gA" target="_blank">Holiday - First of Eleven (Greenday)</a> </td> </tr>
+            <tr> <td>VGM guitarist:</td> <td> <a href="https://www.youtube.com/user/JonnyAtma" target="_blank">GaMetal</a></td> </tr>
+            <tr> <td>VGM mate:</td> <td> <a href="https://www.youtube.com/user/Dethraxx" target="_blank">Dethraxx</a></td> </tr>
+            <tr> <td>VGM song:</td> <td> Currently Spiral Mountain (Banjo Kazooie)</td> </tr>
+            <tr> <td>VGM composer:</td> <td> <a href="https://twitter.com/grantkirkhope" target="_blank">Grant Kirkhope</a></td> </tr>
+            <tr> <td>instrument:</td> <td> MIDI Keyboard</td> </tr>
+            <tr> <td>own song:</td> <td> <a href="https://www.youtube.com/watch?v=yqosH41g8WA">Hopeful</a> </td> </tr>
+            <!-- <tr> <td>games</td> <td><a href="img/5x5 fav games 2021 05 05.jpg" target="_blank">top 25 games</a></td> </tr>
+            <tr> <td>game songs</td> <td><a href="img/5x5 fav vgm.jpg" target="_blank">top 25 game songs</a> </td> </tr> -->
+            <tr> <td>TF2 char:</td> <td> <a href="https://wiki.teamfortress.com/wiki/Engineer" target="_blank">Engineer</a></td> </tr>
+            <tr> <td>TF2 map:</td> <td> <a href="https://wiki.teamfortress.com/wiki/Frontier" target="_blank">Frontier</a></td> </tr>
+            <tr> <td>TF2 song:</td> <td> <a href="https://wiki.teamfortress.com/wiki/RED_Triumphs!_(Soundtrack)" target="_blank">RED Triumphs!</a> </td> </tr>
+            <tr> <td>food and drink:</td> <td> Chili Con Carne + <span alt="von Wesergold Durstlöscher, der einzig Wahre Eistee aus meiner Kindheit :D">Ice Tea</span></td> </tr>
+        </table>
+
+<!-- <div class="gallery">
+    <img src="assets/img/about/about01.jpg">
+    <img src="assets/img/about/about02.jpg">
+    <img src="assets/img/about/about03.jpg">
+    <img src="assets/img/about/about04.jpg">
+    <img src="assets/img/about/about05.png">
+</div> -->
+        

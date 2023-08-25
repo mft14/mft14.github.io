2c565c7c79f2db25fda1d09790d7037b5229fe4e
mft14 <metalfort14@gmail.com>
2023-08-25 08:12:35 +0200

    added upgrade station in VDS

--- src/js/vds.js
+++ src/js/vds.js
@@ -1,6 +1,7 @@
       var songlist  = {
           "vds": [
 //2023
+{"date":"24.08.2023", "vgm":"Team Fortress 2",                           "song":"Upgrade Station",              "link":"jmHMtb_Eq_w", "composer":"Mike Morasky",                       "key":"E Minor",  "bpm":"160", "genre":"Metal",              },
 {"date":"19.08.2023", "vgm":"Left 4 Dead 2",                             "song":"Skin on our teeth",            "link":"UJjTB0A-JtE", "composer":"Mike Morasky",                       "key":"A Minor",  "bpm":"154", "genre":"Hard Rock",          },
 {"date":"11.08.2023", "vgm":"Team Fortress 2",                           "song":"Mannrobics",                   "link":"5Q_4fESOlYM", "composer":"Mike Morasky",                       "key":"G Major",  "bpm":"153", "genre":"EDM Metal",          },
 {"date":"03.08.2023", "vgm":"Team Fortress 2",                           "song":"DeGroot Keep",                 "link":"37AERtJ7q8s", "composer":"Mike Morasky",                       "key":"B Major",  "bpm":"90",  "genre":"Folkmetal",          },

5efba4bdac1660d4d1f9d8b80710ecd6a4368e5c
mft14 <metalfort14@gmail.com>
2024-11-14 11:03:20 +0100

    added nimbasa and tools to menu

--- src/js/vds.js
+++ src/js/vds.js
@@ -3,7 +3,7 @@
 
 //2024
           
-// {"date":".11.2024", "vgm":"Pokemon Black White",          "song":"Nimbasa City","link":"","composer":"Jun Ishikawa<br>Dan Miyakawa",                                    "key":"C Minor",   "bpm":"166",     "genre":"Metal<br>EDM", },
+{"date":"08.11.2024", "vgm":"Pokemon Black White",          "song":"Nimbasa City",       "link":"xBlKWrlRe9w","composer":"Jun Ishikawa<br>Dan Miyakawa",                                    "key":"C Minor",   "bpm":"166",     "genre":"Metal<br>EDM", },
 {"date":"04.10.2024", "vgm":"Pokemon Black White",          "song":"Battle: Team Plasma","link":"YWiTLyVVblw","composer":"Jun Ishikawa<br>Dan Miyakawa",                                    "key":"C Minor",   "bpm":"180",     "genre":"Metal<br>EDM", },
 {"date":"27.09.2024", "vgm":"Kirby: Super Star",            "song":"Gourmet Race",     "link":"ojNpO3DoeHE ","composer":"Jun Ishikawa<br>Dan Miyakawa",                                     "key":"C Minor",   "bpm":"180",     "genre":"Metal<br>EDM", },
 {"date":"22.08.2024", "vgm":"Super Mario World",            "song":"Castle Theme",     "link":"QLe4GTmPqFE", "composer":"Koji Kondo",                                                       "key":"C Minor",   "bpm":"102, 130","genre":"Metal<br>Orchestra", },
